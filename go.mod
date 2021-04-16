0a1a882652dd7138f8e9ae3d620232ab818cab3c
dmorales <dmorales@inespay.com>
2021-04-16 23:44:00 +0200

    first sse chat

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/codehell.net/chat
+
+go 1.16
+
+require github.com/alexandrevicenzi/go-sse v1.6.0
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/alexandrevicenzi/go-sse"
+	"log"
+	"net/http"
+	"time"
+)
+
+var clients []chan string
+
+func main() {
+	s := sse.NewServer(nil)
+	defer s.Shutdown()
+	ms := NewMySSE()
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	http.Handle("/sse", s)
+	http.Handle("/my-sse", ms)
+
+	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+		message := make(map[string]string)
+		err := json.NewDecoder(r.Body).Decode(&message)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		content, ok := message["content"]
+		if ok {
+			s.SendMessage("/sse", sse.SimpleMessage(content))
+			for _, client := range clients {
+				log.Println("message sent to client", client)
+				client <- content
+			}
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type MySSE struct {
+	events []string
+}
+
+func NewMySSE() *MySSE {
+	return &MySSE{
+		events: nil,
+	}
+}
+
+func (s *MySSE) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h := w.Header()
+
+	if r.Method == "GET" {
+		h.Set("Content-Type", "text/event-stream")
+		h.Set("Cache-Control", "no-cache")
+		h.Set("Connection", "keep-alive")
+		h.Set("X-Accel-Buffering", "no")
+		client := make(chan string)
+		clients = append(clients, client)
+		s.events = append(s.events, r.Header.Get("Last-Event-ID"))
+		log.Println(s.events[len(s.events)-1])
+		flusher := w.(http.Flusher)
+		w.WriteHeader(http.StatusOK)
+		flusher.Flush()
+		for {
+			select {
+			case <-r.Context().Done():
+				log.Println("Client closed")
+				return
+			case m := <-client:
+				_, err := fmt.Fprintf(w, "data: %s\n\n", m)
+				if err != nil {
+					log.Println(err)
+				}
+				flusher.Flush()
+				time.Sleep(time.Second * 1)
+			}
+		}
+	}
+}
